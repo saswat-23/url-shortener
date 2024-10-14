c24c1528ec0e06c9718384a183aae116d32cee46
saswat-23 <saswat23@gmail.com>
2024-10-14 20:33:54 +0530

    Spring Security - Basic auth configuration for all endpoints

--- src/main/java/com/saswat23/shorturl/config/AppSecurityConfig.java
+++ src/main/java/com/saswat23/shorturl/config/AppSecurityConfig.java
@@ -2,6 +2,7 @@ package com.saswat23.shorturl.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +13,20 @@ public class AppSecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
+		
+		// Authorize all the Requests
+		httpSecurity.authorizeHttpRequests(request -> {
+			request.anyRequest().authenticated();
+		});
+
+		//Enable BasicAuth - (Using username and pwd from the properties file)
+		httpSecurity.httpBasic(Customizer.withDefaults());
+		
+		// Disable CSRF (to allow Post requests)
+		httpSecurity.csrf(csrfCustomizer -> {
+			csrfCustomizer.disable();
+		});
+		
 		return httpSecurity.build();
 	}
 	
